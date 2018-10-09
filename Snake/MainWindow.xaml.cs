8879578886f521b6e7e793f865033a6478943956
Vladimir <vladimir.kovalyov2002@gmail.com>
2018-10-09 15:38:36 +0500

    f
    f

--- Snake/MainWindow.xaml.cs
+++ Snake/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
 using System.Windows.Threading;
@@ -17,10 +18,14 @@ namespace Snake
         DispatcherTimer timer;
         Random random;
 
+        Point snake;
         Point food;
 
         int width;
         int height;
+
+        int snakeStepX;
+        int snakeStepY;
 
         public MainWindow()
         {
@@ -35,13 +40,46 @@ namespace Snake
 
             timer = new DispatcherTimer();
             timer.Tick += Timer_Tick;
-            timer.Interval = new System.TimeSpan(5000000);
+            timer.Interval = new System.TimeSpan(50000);
             timer.IsEnabled = true;
+            AddFood();
+
+            KeyDown += MainWindow_KeyDown;
+
+            snake = new Point(width / 2,  height / 2);
+            snakeStepX = 0;
+            snakeStepY = 0;
+            MoveSnake();
+
+        }
+
+        private void MainWindow_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Down: snakeStepX = 0; snakeStepY = +1; break;
+                case Key.Up: snakeStepX = 0; snakeStepY = -1; break;
+                case Key.Left: snakeStepX = -1; snakeStepY = 0; break;
+                case Key.Right: snakeStepX = 1; snakeStepY = 0; break;
+            }
         }
 
         private void Timer_Tick(object sender, System.EventArgs e)
         {
-            AddFood();
+            MoveSnake();
+        }
+
+        void MoveSnake()
+        {
+            snake.X += snakeStepX;
+            snake.Y += snakeStepY;
+
+            Ellipse ellipse = CreateEllipse(snake, Brushes.HotPink);
+            if (Map.Children.Count > 1)
+            {
+                Map.Children.RemoveAt(1);
+            }
+            Map.Children.Insert(1, ellipse);
         }
 
         void AddFood()
